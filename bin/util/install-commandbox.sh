658e07b518adcf81981423f14bdd8ab92fdd4571
Jon Clausen <jon_clausen@silowebworks.com>
2017-07-06 21:33:44 -0400

    fix commandbox install

--- bin/util/install-commandbox.sh
+++ bin/util/install-commandbox.sh
@@ -2,5 +2,5 @@
 
 # Installs the latest CommandBox Binary
 curl --location 'https://www.ortussolutions.com/parent/download/commandbox/type/bin' -o /tmp/box.zip
-unzip /tmp/box.zip -d ${BIN_DIR} && chmod +x ${BIN_DIR}/box
+unzip /tmp/box.zip -d ${BIN_DIR}/bin && chmod +x ${BIN_DIR}/bin/box
 echo "$(box version) successfully installed"
